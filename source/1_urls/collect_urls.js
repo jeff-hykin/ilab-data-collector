251a9a372b70c2b3c7345c08f29d67b8662a5946
Jeff Hykin <jeff.hykin@gmail.com>
2020-11-19 12:35:43 -0600

    add url collector

---
+++ source/1_urls/collect_urls.js
@@ -0,0 +1,63 @@
+const got = require('got')
+const fs = require("fs")
+
+const pathToMemory = __dirname+"/memory.json"
+
+setTimeout(async () => {
+    let memory = JSON.parse(fs.readFileSync(pathToMemory))
+    
+    while (true) {
+
+        // pick a random url
+        let urls = Object.keys(memory.urls)
+        console.log(`${urls.length} urls`)
+        let randomUrl = urls[Math.floor(Math.random() * urls.length)]
+        let html = await getHtmlFor(randomUrl)
+
+
+        // 
+        // extract video id's
+        // 
+        let videoIds = findAll(
+            /href="(?:https:\/\/youtu\.be\/|(?:https:\/\/www\.youtube\.com)?\/watch\?v=)([a-zA-Z0-9_.]{11})(?:"|&)/, html
+        ).map(each=>each[1])
+        let videoIdMatch = html.match(/videoId:"(.+?)"/)
+        if (videoIdMatch) {
+            if (videoIdMatch[1].length == 11) {
+                videoIds.push(videoIdMatch[1])
+            }
+        }
+
+        // 
+        // save to file
+        //
+        let newUrls = videoIds.map(each=>`https://youtu.be/${each}`)
+        for (let each of newUrls ) { memory.urls[each]     = {} }
+        for (let each of videoIds) { memory.videoIds[each] = {} }
+        fs.writeFileSync(pathToMemory, JSON.stringify(memory,0,4))
+    }
+}, 0)
+
+
+async function getHtmlFor(url) {
+    let html = ""
+    try {
+        const response = await got(url)
+        html = response.body
+    } catch (error) {}
+    return html
+}
+
+function findAll(regexPattern, sourceString) {
+    let output = []
+    let match
+    // make sure the pattern has the global flag
+    let regexPatternWithGlobal = RegExp(regexPattern,[...new Set("g"+regexPattern.flags)].join(""))
+    while (match = regexPatternWithGlobal.exec(sourceString)) {
+        // get rid of the string copy
+        delete match.input
+        // store the match data
+        output.push(match)
+    } 
+    return output
+}
---
+++ source/3_happy_observations/collect_happy_observations.py
@@ -0,0 +1,18 @@
+from toolbox.file_system_tools import FS
+
+# 
+# Download videos
+# 
+
+
+# 
+# Scan them
+# 
+
+# 
+# Use algorithm for finding moments
+# 
+
+# 
+# Save to json
+# 
